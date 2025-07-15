13cc4a7dd88247ba2260a6b3bc1b4ce3e52001aa
saloni <salonijaiswal.7412@gmail.com>
2025-07-15 17:30:53 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=' w-screen h-screen p-12 '>
+      <h1 className="text-4xl font-bold text-indigo-600">Hello Journiva 🌙</h1>
+      
+    </div>
+  )
+}
+
+export default App
