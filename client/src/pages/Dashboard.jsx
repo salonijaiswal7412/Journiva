9ef5aaba295f2a608f056d5c855023caeb963c0c
saloni <salonijaiswal.7412@gmail.com>
2025-08-15 22:00:42 +0530

    modified dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='bg-white w-screen h-screen p-6'>
-        <nav className='h-10 bg-'></nav>
+        <nav className='h-10 bg-'>
+          <h1> Journiva </h1>
+        </nav>
     </div>
   )
 }
